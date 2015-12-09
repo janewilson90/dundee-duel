25dff1a1df689d5315a6a674987fe77851e23311
Jane Wilson <jane.wilson@evogi.coim>
2015-12-09 11:26:59 +0000

    How it works fixed

--- how-it-works.php
+++ how-it-works.php
@@ -14,22 +14,22 @@
               <div class="collapsible-header"><i class="material-icons">today</i>On the day</div>
               <div class="collapsible-body"><p>First off, fence your team.</p>
               <p>Secondly, find a team you want to fence and a piste and fence that team.</p>
-              <p>Keep going like that until either you've fenced all the teams or you run out of time.<p></div>
+              <p>Keep going like that until either you've fenced all the teams or you run out of time (bouts last one minute).<p></div>
             </li>
             <li>
               <div class="collapsible-header"><i class="material-icons">live_help</i>How the scoring works</div>
               <div class="collapsible-body"><p>You're only fencing to one hit so matches tend to go quickly.</p>
-              <p>If you get a double hit, mark it on the score sheet as "DD".</p>
+              <p>If you get a double hit, pretend it never happened. Keep fencing until someone scores a single hit or time runs out.</p>
               <p>If you run out of time, mark it on the score sheet as "T".</p>
               <p><strong>The score sheets work the same way as normal poule sheets!</strong></div>
             </li>
             <li>
             <div class="collapsible-header"><i class="material-icons">perm_identity</i>Individual rankings</div>
-              <div class="collapsible-body"><p>Your individual scores are tallied up. The more outright victories you have, the higher up the rankings you go. If you and another fencer have the same number of victories, then the fencer with the least "double defeats" is ranked higher.</p></div>
+              <div class="collapsible-body"><p>Your individual scores are tallied up. The more outright victories you have, the higher up the rankings you go. If you and another fencer have the same number of victories, then the fencer with the least "time outs" is ranked higher.</p></div>
             </li>
             <li>
               <div class="collapsible-header"><i class="material-icons">group</i>Team rankings</div>
-              <div class="collapsible-body"><p>The more teams your team beats, the higher up the rankings you go!</p></div>
+              <div class="collapsible-body"><p>Team scores are an agregation of individual scores. So member1 plus member2 plus member3 scores gives the teams score. The higher the score, the higher the team ranking.</p></div>
             </li>
           </ul>
         </div>
